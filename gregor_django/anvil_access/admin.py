b7dd787fef3382607a525db45be72b19b9327afb
Adrienne Stilp <amstilp@uw.edu>
2022-06-22 15:53:56 -0700

    Add admin classes for anvil_access app

--- gregor_django/anvil_access/admin.py
+++ gregor_django/anvil_access/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.ConsentGroup)
+class ConsentGroupAdmin(admin.ModelAdmin):
+    """Admin class for the ConsentGroup model."""
+
+    list_display = ("code",)
+    search_fields = (
+        "code",
+        "data_use_limitations",
+    )
+
+
+@admin.register(models.ResearchCenter)
+class ResearchCenterAdmin(admin.ModelAdmin):
+    """Admin class for the ResearchCenter model."""
+
+    list_display = (
+        "short_name",
+        "full_name",
+    )
+    search_fields = (
+        "short_name",
+        "full_name",
+    )
+
+
+@admin.register(models.WorkspaceData)
+class WorkspaceDataAdmin(admin.ModelAdmin):
+    """Admin class for the WorkspaceData model."""
+
+    list_display = (
+        "workspace",
+        "research_center",
+        "consent_group",
+        "version",
+    )
+    list_filter = ("research_center", "consent_group")
