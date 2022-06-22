ca374f238f7dd05f91e18116a67786fb9dc457b7
Adrienne Stilp <amstilp@uw.edu>
2022-06-22 14:25:23 -0700

    Add a ResearchCenter model

--- gregor_django/anvil_access/tests/test_models.py
+++ gregor_django/anvil_access/tests/test_models.py
@@ -7,6 +7,8 @@ from . import factories
 
 
 class ConsentGroupTest(TestCase):
+    """Tests for the ConsentGroup model."""
+
     def test_model_saving(self):
         """Creation using the model constructor and .save() works."""
         instance = models.ConsentGroup(code=models.ConsentGroup.GRU, data_use_limitations="test limitations")
@@ -40,3 +42,34 @@ class ConsentGroupTest(TestCase):
         instance = models.ConsentGroup(code="FOO", data_use_limitations="test limitations")
         with self.assertRaises(ValidationError):
             instance.full_clean()
+
+
+class ResearchCenterTest(TestCase):
+    """Tests for the ResearchCenter model."""
+
+    def test_model_saving(self):
+        """Creation using the model constructor and .save() works."""
+        instance = models.ResearchCenter(full_name="Test name", short_name="TEST")
+        instance.save()
+        self.assertIsInstance(instance, models.ResearchCenter)
+
+    def test_str_method(self):
+        """The custom __str__ method returns the correct string."""
+        instance = factories.ResearchCenterFactory.create(short_name="Test")
+        instance.save()
+        self.assertIsInstance(instance.__str__(), str)
+        self.assertEqual(instance.__str__(), "Test")
+
+    # def test_get_absolute_url(self):
+    #     """The get_absolute_url() method works."""
+    #     instance = factories.ConsentGroupFactory()
+    #     self.assertIsInstance(instance.get_absolute_url(), str)
+
+    def test_unique_code(self):
+        """Saving a model with a duplicate short name fails."""
+        instance1 = factories.ResearchCenterFactory.create(short_name="FOO")
+        instance2 = models.ResearchCenter(short_name="FOO", full_name="full name")
+        with self.assertRaises(ValidationError):
+            instance2.full_clean()
+        with self.assertRaises(IntegrityError):
+            instance2.save()
