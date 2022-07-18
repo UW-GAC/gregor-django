ff42a773ea9b49fe6e63e4ad4e7aedc33a1c27d3
Adrienne Stilp <amstilp@uw.edu>
2022-07-18 16:13:24 -0700

    Add a model to track Research Centers

--- gregor_django/gregor_anvil/models.py
+++ gregor_django/gregor_anvil/models.py
@@ -19,3 +19,21 @@ class ConsentGroup(models.Model):
             A string showing the short consent code of the object.
         """
         return self.code
+
+
+class ResearchCenter(models.Model):
+    """A model to track Research Centers."""
+
+    short_name = models.CharField(max_length=15, unique=True)
+    """The short name of the Research Center."""
+
+    full_name = models.CharField(max_length=255)
+    """The full name of the Research Center."""
+
+    def __str__(self):
+        """String method.
+
+        Returns:
+            A string showing the short name of the object.
+        """
+        return self.short_name
