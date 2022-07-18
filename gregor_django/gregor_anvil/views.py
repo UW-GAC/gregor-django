5d56cdb6f528fb353fd33b8498728db85cd26a2a
Adrienne Stilp <amstilp@uw.edu>
2022-07-18 16:52:25 -0700

    Add basic list and detail views for ResearchCenters

--- gregor_django/gregor_anvil/views.py
+++ gregor_django/gregor_anvil/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from anvil_consortium_manager.auth import AnVILConsortiumManagerViewRequired
+from django.views.generic import DetailView, ListView
+
+from . import models
+
+
+class ResearchCenterDetail(AnVILConsortiumManagerViewRequired, DetailView):
+    """View to show details about a `ResearchCenter`."""
+
+    model = models.ResearchCenter
+
+
+class ResearchCenterList(AnVILConsortiumManagerViewRequired, ListView):
+    """View to show a list of `ResearchCenters`."""
+
+    model = models.ResearchCenter
