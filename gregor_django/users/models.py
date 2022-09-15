823968f397aee04df91f5914b3a8a26557c707e3
Jonas Carson <jcarson@uw.edu>
2022-09-15 12:23:27 -0700

    Update user profile to include user research centers, update on user login, update displayed profile data

--- gregor_django/users/models.py
+++ gregor_django/users/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db.models import CharField, ManyToManyField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
+from gregor_django.gregor_anvil.models import ResearchCenter
 
 
 class User(AbstractUser):
@@ -11,6 +13,7 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    research_centers = ManyToManyField(ResearchCenter)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
