9bc27eb0af7cc2b24a44db8aad1e7df4b17fc006
Adrienne Stilp <amstilp@uw.edu>
2023-01-23 16:47:03 -0800

    Add a skeleton custom Account adapter for GREGoR

--- gregor_django/gregor_anvil/adapters.py
+++ gregor_django/gregor_anvil/adapters.py
@@ -1,7 +1,29 @@
+from anvil_consortium_manager.adapters.account import BaseAccountAdapter
 from anvil_consortium_manager.adapters.workspace import BaseWorkspaceAdapter
-from anvil_consortium_manager.tables import WorkspaceTable
+from anvil_consortium_manager.tables import AccountTable, WorkspaceTable
 
 from . import forms, models, tables
+
+# from django.db.models import Q
+
+
+class AccountAdapter(BaseAccountAdapter):
+    """Custom account adapter for PRIMED."""
+
+    list_table_class = AccountTable
+
+    # def get_autocomplete_queryset(self, queryset, q):
+    #     """Filter to Accounts where the email or the associated user name matches the query `q`."""
+    #     queryset = queryset.filter(Q(email__icontains=q) | Q(user__name__icontains=q))
+    #     return queryset
+    #
+    # def get_autocomplete_label(self, account):
+    #     """Adapter to provide a label for an account in autocomplete views."""
+    #     if account.user:
+    #         name = account.user.name
+    #     else:
+    #         name = "---"
+    #     return "{} ({})".format(name, account.email)
 
 
 class UploadWorkspaceAdapter(BaseWorkspaceAdapter):
