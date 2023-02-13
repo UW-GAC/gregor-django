0c39b1b7193ce289f37beed5d6cb563b52dfbba9
Adrienne Stilp <amstilp@uw.edu>
2023-02-13 14:14:57 -0800

    Add an adapter for CombinedConsortiumDataWorkspace and register it

--- config/settings/base.py
+++ config/settings/base.py
@@ -359,9 +359,10 @@ DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap4.html"
 # Specify the path to the service account to use for managing access on AnVIL.
 ANVIL_API_SERVICE_ACCOUNT_FILE = env("ANVIL_API_SERVICE_ACCOUNT_FILE")
 ANVIL_WORKSPACE_ADAPTERS = [
+    "gregor_django.gregor_anvil.adapters.ExampleWorkspaceAdapter",
     "gregor_django.gregor_anvil.adapters.TemplateWorkspaceAdapter",
     "gregor_django.gregor_anvil.adapters.UploadWorkspaceAdapter",
-    "gregor_django.gregor_anvil.adapters.ExampleWorkspaceAdapter",
+    "gregor_django.gregor_anvil.adapters.CombinedConsortiumDataWorkspaceAdapter",
 ]
 ANVIL_ACCOUNT_ADAPTER = "gregor_django.gregor_anvil.adapters.AccountAdapter"
 
