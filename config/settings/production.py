289ca3a1e5dc3f30f445585873459a3217932782
Jonas Carson <jcarson@uw.edu>
2023-05-16 08:05:01 -0700

    Disable HSTS header. Dupe of apache config

--- config/settings/production.py
+++ config/settings/production.py
@@ -53,8 +53,9 @@ SESSION_COOKIE_AGE = 86400
 CSRF_COOKIE_SECURE = True
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
-# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
-SECURE_HSTS_SECONDS = 60
+# Disabling this by setting to 0 as this header is already in place
+# in our apache configuration. Having in both places causes duplicate header
+SECURE_HSTS_SECONDS = 0
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
     "DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True
