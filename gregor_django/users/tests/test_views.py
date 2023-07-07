6287913fc3b04cd4138ba6f15a7e1668b29d971f
Adrienne Stilp <amstilp@uw.edu>
2023-07-07 16:19:15 -0700

    Add not logged in test for UserSearchFormView

--- gregor_django/users/tests/test_views.py
+++ gregor_django/users/tests/test_views.py
@@ -15,7 +15,12 @@ from django.urls import reverse
 
 from gregor_django.users.forms import UserChangeForm
 from gregor_django.users.tests.factories import UserFactory
-from gregor_django.users.views import UserRedirectView, UserUpdateView, user_detail_view
+from gregor_django.users.views import (
+    UserRedirectView,
+    UserUpdateView,
+    user_detail_view,
+    user_search_form_view,
+)
 
 pytestmark = pytest.mark.django_db(transaction=True)
 
@@ -124,7 +129,19 @@ class UserAutocompleteViewTest(TestCase):
         """Get the url for the view being tested."""
         return reverse("users:autocomplete", args=args)
 
-    def test_status_code_with_user_permission(self):
+    def get_view(self):
+        """Return the view being tested."""
+        return user_search_form_view
+
+    def test_view_redirect_not_logged_in(self):
+        "View redirects to login view when user is not logged in."
+        # Need a client for redirects.
+        response = self.client.get(self.get_url())
+        self.assertRedirects(
+            response, resolve_url(settings.LOGIN_URL) + "?next=" + self.get_url()
+        )
+
+    def test_status_code_logged_in(self):
         """Returns successful response code."""
         self.client.force_login(self.user)
         response = self.client.get(self.get_url())
