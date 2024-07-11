72a30dcb6bcc655c857e1483b39afe2877b992bc
Adrienne Stilp <amstilp@uw.edu>
2024-07-11 14:28:50 -0700

    Add the crontab file to the activate script
    That we can we can reference it with an environment variable when
    deploying.

--- gregor-apps-dev-activate.sh
+++ gregor-apps-dev-activate.sh
@@ -6,6 +6,8 @@ export DJANGO_SITE_DIR=/var/www/django/gregor_apps_dev/
 export DJANGO_SITE_USER=gregorweb
 export DJANGO_SETTINGS_MODULE=config.settings.apps_dev
 export DJANGO_WSGI_FILE=config/apps_dev_wsgi.py
+export DJANGO_CRONTAB=gregor_apps_dev.cron
 export GAC_ENV=gregor_apps_dev
+
 cd $DJANGO_SITE_DIR
 . venv/bin/activate
