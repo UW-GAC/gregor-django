4bc0d91cea7206ee4e789e8ae8530d8e56f22e96
Adrienne Stilp <amstilp@uw.edu>
2024-08-28 11:29:04 -0700

    Start a mangement command for auditing upload workspaces

---
+++ gregor_django/gregor_anvil/management/commands/run_upload_workspace_audit.py
@@ -0,0 +1,65 @@
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
+from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
+
+from ...audit import upload_workspace_sharing_audit
+
+
+class Command(BaseCommand):
+    help = "Run access audits on UploadWorkspace."
+
+    def add_arguments(self, parser):
+        email_group = parser.add_argument_group(title="Email reports")
+        email_group.add_argument(
+            "--email",
+            help="""Email to which to send audit reports that need action or have errors.""",
+        )
+
+    def run_sharing_audit(self, *args, **options):
+        self.stdout.write("Running UploadWorkspace sharing audit... ", ending="")
+        audit = upload_workspace_sharing_audit.UploadWorkspaceSharingAudit()
+        audit.run_audit()
+        self._handle_audit_results(audit, "", **options)
+
+    def _handle_audit_results(self, audit, url, **options):
+        # Report errors and needs access.
+        audit_ok = audit.ok()
+        # Construct the url for handling errors.
+        url = "https://" + Site.objects.get_current().domain + url
+        if audit_ok:
+            self.stdout.write(self.style.SUCCESS("ok!"))
+        else:
+            self.stdout.write(self.style.ERROR("problems found."))
+
+        # Print results
+        self.stdout.write("* Verified: {}".format(len(audit.verified)))
+        self.stdout.write("* Needs action: {}".format(len(audit.needs_action)))
+        self.stdout.write("* Errors: {}".format(len(audit.errors)))
+
+        if not audit_ok:
+            self.stdout.write(self.style.ERROR(f"Please visit {url} to resolve these issues."))
+
+            # Send email if requested and there are problems.
+            email = options["email"]
+            subject = "{} - problems found".format(audit.__class__.__name__)
+            html_body = render_to_string(
+                "gregor_anvil/email_audit_report.html",
+                context={
+                    "title": "Upload workspace audit",
+                    "audit_results": audit,
+                    "url": url,
+                },
+            )
+            send_mail(
+                subject,
+                "Audit problems found. Please see attached report.",
+                None,
+                [email],
+                fail_silently=False,
+                html_message=html_body,
+            )
+
+    def handle(self, *args, **options):
+        self.run_sharing_audit(*args, **options)
+        # self.run_auth_domain_audit(*args, **options)
---
+++ gregor_django/gregor_anvil/tests/test_commands.py
@@ -0,0 +1,300 @@
+"""Tests for management commands in the `gregor_anvil` app."""
+
+from io import StringIO
+
+from anvil_consortium_manager.models import WorkspaceGroupSharing
+from anvil_consortium_manager.tests.factories import (
+    GroupGroupMembershipFactory,
+    WorkspaceGroupSharingFactory,
+)
+from django.core import mail
+from django.core.management import call_command
+from django.test import TestCase
+
+from . import factories
+
+
+class RunUploadWorkspaceAuditTest(TestCase):
+    """Tests for the run_upload_workspace_audit command"""
+
+    def test_no_upload_workspaces(self):
+        """Test command output."""
+        out = StringIO()
+        call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+        expected_string = "\n".join(
+            [
+                "Running UploadWorkspace sharing audit... ok!",
+                "* Verified: 0",
+                "* Needs action: 0",
+                "* Errors: 0",
+            ]
+        )
+        self.assertIn(expected_string, out.getvalue())
+        expected_string = "\n".join(
+            [
+                "Running UploadWorkspace auth domain audit... ok!",
+                "* Verified: 0",
+                "* Needs action: 0",
+                "* Errors: 0",
+            ]
+        )
+        self.assertIn(expected_string, out.getvalue())
+        # Zero messages have been sent by default.
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_sharing_audit_one_instance_verified(self):
+        """Test command output with one verified instance."""
+        # Create a workspace and matching DAR.
+        workspace = factories.UploadWorkspaceFactory.create()
+        WorkspaceGroupSharingFactory.create(
+            workspace=workspace.workspace,
+            group=workspace.workspace.authorization_domains.first(),
+        )
+        out = StringIO()
+        call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+        expected_string = "\n".join(
+            [
+                "Running UploadWorkspace sharing audit... ok!",
+                "* Verified: 1",
+                "* Needs action: 0",
+                "* Errors: 0",
+            ]
+        )
+        self.assertIn(expected_string, out.getvalue())
+        # Zero messages have been sent by default.
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_access_audit_one_instance_needs_action(self):
+        """Test command output with one needs_action instance."""
+        # Create a workspace and matching DAR.
+        factories.UploadWorkspaceFactory.create()
+        out = StringIO()
+        call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+        expected_string = "\n".join(
+            [
+                "Running UploadWorkspace sharing audit... problems found.",
+                "* Verified: 0",
+                "* Needs action: 1",
+                "* Errors: 0",
+            ]
+        )
+        self.assertIn(expected_string, out.getvalue())
+        # Zero messages have been sent by default.
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_access_audit_one_instance_error(self):
+        """Test command output with one error instance."""
+        workspace = factories.UploadWorkspaceFactory.create()
+        WorkspaceGroupSharingFactory.create(
+            workspace=workspace.workspace,
+            group=workspace.workspace.authorization_domains.first(),
+            access=WorkspaceGroupSharing.OWNER,
+        )
+        out = StringIO()
+        call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+        expected_string = "\n".join(
+            [
+                "Running UploadWorkspace sharing audit... problems found.",
+                "* Verified: 0",
+                "* Needs action: 0",
+                "* Errors: 1",
+            ]
+        )
+        self.assertIn(expected_string, out.getvalue())
+        # Zero messages have been sent by default.
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_access_audit_one_instance_verified_email(self):
+        """No email is sent when there are no errors."""
+        # Create a workspace and matching DAR.
+        factories.dbGaPWorkspaceFactory.create()
+        factories.dbGaPApplicationFactory.create()
+        out = StringIO()
+        call_command("run_upload_workspace_audit", "--no-color", email="test@example.com", stdout=out)
+        self.assertIn("Running UploadWorkspace sharing audit... ok!", out.getvalue())
+        # Zero messages have been sent by default.
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_access_audit_one_instance_needs_action_email(self):
+        """Email is sent for one needs_action instance."""
+        # Create a workspace and matching DAR.
+        dbgap_workspace = factories.dbGaPWorkspaceFactory.create()
+        factories.dbGaPDataAccessRequestForWorkspaceFactory.create(dbgap_workspace=dbgap_workspace)
+        out = StringIO()
+        call_command("run_upload_workspace_audit", "--no-color", email="test@example.com", stdout=out)
+        expected_string = "\n".join(
+            [
+                "Running UploadWorkspace sharing audit... problems found.",
+                "* Verified: 0",
+                "* Needs action: 1",
+                "* Errors: 0",
+            ]
+        )
+        self.assertIn(expected_string, out.getvalue())
+        # One message has been sent by default.
+        self.assertEqual(len(mail.outbox), 1)
+        email = mail.outbox[0]
+        self.assertEqual(email.to, ["test@example.com"])
+        self.assertEqual(email.subject, "dbGaPAccessAudit - problems found")
+
+    def test_access_audit_one_instance_error_email(self):
+        """Test command output with one error instance."""
+        # Create a workspace and matching DAR.
+        dbgap_workspace = factories.dbGaPWorkspaceFactory.create()
+        dbgap_application = factories.dbGaPApplicationFactory.create()
+        GroupGroupMembershipFactory(
+            parent_group=dbgap_workspace.workspace.authorization_domains.first(),
+            child_group=dbgap_application.anvil_access_group,
+        )
+        out = StringIO()
+        call_command("run_upload_workspace_audit", "--no-color", email="test@example.com", stdout=out)
+        expected_string = "\n".join(
+            [
+                "Running UploadWorkspace sharing audit... problems found.",
+                "* Verified: 0",
+                "* Needs action: 0",
+                "* Errors: 1",
+            ]
+        )
+        self.assertIn(expected_string, out.getvalue())
+        # One message has been sent by default.
+        self.assertEqual(len(mail.outbox), 1)
+        email = mail.outbox[0]
+        self.assertEqual(email.to, ["test@example.com"])
+        self.assertEqual(email.subject, "dbGaPAccessAudit - problems found")
+
+    # def test_access_audit_different_domain(self):
+    #     """Test command output when a different domain is specified."""
+    #     site = Site.objects.create(domain="foobar.com", name="test")
+    #     site.save()
+    #     with self.settings(SITE_ID=site.id):
+    #         dbgap_workspace = factories.dbGaPWorkspaceFactory.create()
+    #         factories.dbGaPDataAccessRequestForWorkspaceFactory.create(dbgap_workspace=dbgap_workspace)
+    #         out = StringIO()
+    #         call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+    #         self.assertIn("Running UploadWorkspace sharing audit... problems found.", out.getvalue())
+    #         self.assertIn("https://foobar.com", out.getvalue())
+
+    # def test_collaborator_audit_one_instance_verified(self):
+    #     """Test command output with one verified instance."""
+    #     # Create a workspace and matching DAR.
+    #     factories.dbGaPApplicationFactory.create()
+    #     # Verified no access for PI.
+    #     out = StringIO()
+    #     call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+    #     expected_string = "\n".join(
+    #         [
+    #             "Running dbGaP collaborator audit... ok!",
+    #             "* Verified: 1",
+    #             "* Needs action: 0",
+    #             "* Errors: 0",
+    #         ]
+    #     )
+    #     self.assertIn(expected_string, out.getvalue())
+    #     # Zero messages have been sent by default.
+    #     self.assertEqual(len(mail.outbox), 0)
+
+    # def test_collaborator_audit_one_instance_needs_action(self):
+    #     """Test command output with one needs_action instance."""
+    #     application = factories.dbGaPApplicationFactory.create()
+    #     AccountFactory.create(user=application.principal_investigator)
+    #     out = StringIO()
+    #     call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+    #     expected_string = "\n".join(
+    #         [
+    #             "Running dbGaP collaborator audit... problems found.",
+    #             "* Verified: 0",
+    #             "* Needs action: 1",
+    #             "* Errors: 0",
+    #         ]
+    #     )
+    #     self.assertIn(expected_string, out.getvalue())
+    #     # Zero messages have been sent by default.
+    #     self.assertEqual(len(mail.outbox), 0)
+
+    # def test_collaborator_audit_one_instance_error(self):
+    #     """Test command output with one error instance."""
+    #     application = factories.dbGaPApplicationFactory.create()
+    #     GroupGroupMembershipFactory(
+    #         parent_group=application.anvil_access_group,
+    #     )
+    #     out = StringIO()
+    #     call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+    #     expected_string = "\n".join(
+    #         [
+    #             "Running dbGaP collaborator audit... problems found.",
+    #             "* Verified: 1",  # PI - no linked account, verified no access.
+    #             "* Needs action: 0",
+    #             "* Errors: 1",
+    #         ]
+    #     )
+    #     self.assertIn(expected_string, out.getvalue())
+    #     # Zero messages have been sent by default.
+    #     self.assertEqual(len(mail.outbox), 0)
+
+    # def test_collaborator_audit_one_instance_verified_email(self):
+    #     """No email is sent when there are no errors."""
+    #     factories.dbGaPApplicationFactory.create()
+    #     # Verified no access for PI.
+    #     out = StringIO()
+    #     call_command("run_upload_workspace_audit", "--no-color", email="test@example.com", stdout=out)
+    #     self.assertIn("Running dbGaP collaborator audit... ok!", out.getvalue())
+    #     # Zero messages have been sent by default.
+    #     self.assertEqual(len(mail.outbox), 0)
+
+    # def test_collaborator_audit_one_instance_needs_action_email(self):
+    #     """Email is sent for one needs_action instance."""
+    #     # Create a workspace and matching DAR.
+    #     application = factories.dbGaPApplicationFactory.create()
+    #     AccountFactory.create(user=application.principal_investigator)
+    #     out = StringIO()
+    #     call_command("run_upload_workspace_audit", "--no-color", email="test@example.com", stdout=out)
+    #     expected_string = "\n".join(
+    #         [
+    #             "Running dbGaP collaborator audit... problems found.",
+    #             "* Verified: 0",
+    #             "* Needs action: 1",
+    #             "* Errors: 0",
+    #         ]
+    #     )
+    #     self.assertIn(expected_string, out.getvalue())
+    #     # One message has been sent by default.
+    #     self.assertEqual(len(mail.outbox), 1)
+    #     email = mail.outbox[0]
+    #     self.assertEqual(email.to, ["test@example.com"])
+    #     self.assertEqual(email.subject, "dbGaPCollaboratorAudit - problems found")
+
+    # def test_collaborator_audit_one_instance_error_email(self):
+    #     """Test command output with one error instance."""
+    #     application = factories.dbGaPApplicationFactory.create()
+    #     GroupGroupMembershipFactory(
+    #         parent_group=application.anvil_access_group,
+    #     )
+    #     out = StringIO()
+    #     call_command("run_upload_workspace_audit", "--no-color", email="test@example.com", stdout=out)
+    #     expected_string = "\n".join(
+    #         [
+    #             "Running dbGaP collaborator audit... problems found.",
+    #             "* Verified: 1",  # PI - no linked account, verified no access.
+    #             "* Needs action: 0",
+    #             "* Errors: 1",
+    #         ]
+    #     )
+    #     self.assertIn(expected_string, out.getvalue())
+    #     # One message has been sent by default.
+    #     self.assertEqual(len(mail.outbox), 1)
+    #     email = mail.outbox[0]
+    #     self.assertEqual(email.to, ["test@example.com"])
+    #     self.assertEqual(email.subject, "dbGaPCollaboratorAudit - problems found")
+
+    # def test_collaborator_audit_different_domain(self):
+    #     """Test command output when a different domain is specified."""
+    #     site = Site.objects.create(domain="foobar.com", name="test")
+    #     site.save()
+    #     with self.settings(SITE_ID=site.id):
+    #         application = factories.dbGaPApplicationFactory.create()
+    #         AccountFactory.create(user=application.principal_investigator)
+    #         out = StringIO()
+    #         call_command("run_upload_workspace_audit", "--no-color", stdout=out)
+    #         self.assertIn("Running dbGaP collaborator audit... problems found.", out.getvalue())
+    #         self.assertIn("https://foobar.com", out.getvalue())
